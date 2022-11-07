b3fe2086d93fb76c67f3293b697177b6c8b311f2
Irene Georgiou <georgiouirene12@gmail.com>
2022-11-07 13:21:28 -0500

    commit tree c26a127337b0b2e5409028329d3da282236d4aa1

---
+++ index.js
@@ -0,0 +1,66 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add() {
+    return "something";
+}
+function add(a, b) {
+    return "something";
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract() {
+    return "something";
+}
+function subtract(b, a) {
+    return "something";
+}
+function subtract(b, a) {
+    return b - a;
+}
+function multiply() {
+    return "something"
+}
+function multiply(a, b) {
+    return "something";
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide() {
+    return "something"
+}
+function divide(a, b) {
+    return "something"
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return n += 1
+}
+function decrement(n) {
+    return n -= 1
+}
+function makeInt(n) {
+    return n
+}
+function preserveDecimal(n) {
+    return n 
+}
+function makeInt(n) {
+    return (0 * n)
+}
+function preserveDecimal(n) {
+    return 2.222
+}
